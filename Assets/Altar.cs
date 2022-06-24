fd214b49078718f550983a0732111ce29db94ae6
Paulo Salvatore <salvatoregames@gmail.com>
2022-06-24 10:36:43 -0300

    Ajustes nas layers e script de altar

---
+++ Assets/Altar.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Altar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        print(other.name);
+    }
+}
